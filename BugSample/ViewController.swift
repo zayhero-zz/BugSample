6376e04b23860f976b7a5a16e1d7a2c871b73b54
brian_zhang <brian.zhang@airbnb.com>
2017-10-17 10:37:09 -0700

    re-gen project

---
+++ BugSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BugSample
+//
+//  Created by brian_zhang on 10/17/17.
+//  Copyright © 2017 brian_zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+use_frameworks!
+
+def PromiseKit
+  pod "PromiseKit/CorePromise", '4.0.5'
+  pod "PromiseKit/CoreLocation", '4.0.5'
+end
+
+target 'BugSample' do
+  project 'BugSample.xcodeproj'
+
+  PromiseKit()
+end
